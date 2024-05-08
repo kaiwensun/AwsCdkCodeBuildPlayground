06dfe8e33e7807099881b7b24ec5fbf50124023f
Kaiwen Sun <skw_kevin@126.com>
2024-05-07 22:58:36 -0700

    init: cdk init app --language typescript

---
+++ test/code_build_playground.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CodeBuildPlayground from '../lib/code_build_playground-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/code_build_playground-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CodeBuildPlayground.CodeBuildPlaygroundStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
